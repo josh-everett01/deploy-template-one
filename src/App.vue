041d3e795e7ec1952958b484854c626c48d2a7f0
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2020-11-11 14:05:26 -0600

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/stealie.png" />
+    <img src="./assets/tealie.png" />
     <router-view />
   </div>
 </template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@
         <a href="https://vuejs.org" target="_blank"> Core Docs </a>
       </li>
       <li>
-        <a hr!ef="https://forum.vuejs.org" target="_blank"> Forum </a>
+        <a href="https://forum.vuejs.org" target="_blank"> Forum </a>
       </li>
       <li>
         <a href="https://chat.vuejs.org" target="_blank"> Community Chat </a>
